6ade28786018b480e668cb902344ab8cd228902a
April Cook <ocitypromo@gmail.com>
2022-08-05 11:02:04 -0500

    removed white space and all tests ran successfully

--- SpecFlowFirstScenario/StepDefinitions/RetrieveLocationDataStepDefinitions.cs
+++ SpecFlowFirstScenario/StepDefinitions/RetrieveLocationDataStepDefinitions.cs
@@ -9,25 +9,21 @@ namespace SpecFlowFirstScenario.StepDefinitions
         [Given(@"the country code us and zip code (.*)")]
         public void GivenTheCountryCodeUsAndZipCode(int p0)
         {
-           
         }
 
         [When(@"I request the locations corresponding to these codes")]
         public void WhenIRequestTheLocationsCorrespondingToTheseCodes()
-        {
-            
+        { 
         }
 
         [Then(@"the response contains the place name Beverly Hills")]
         public void ThenTheResponseContainsThePlaceNameBeverlyHills()
         {
-           
         }
 
         [Then(@"the response contains exactly (.*) locaton")]
         public void ThenTheResponseContainsExactlyLocaton(int p0)
         {
-           
         }
     }
 }
